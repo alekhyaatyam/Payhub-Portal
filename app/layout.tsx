e78279e4ae011d670e6f989a28e61d53e8f6fd98
Sampath <Sampath@example.com>
2026-03-30 22:35:27 +0100

    Version 4.0 improved mobile responsiveness and header link spacing

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import Footer from "@/components/Footer";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>
         <NavBar />
 
